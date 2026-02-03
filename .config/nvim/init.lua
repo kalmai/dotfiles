df60d9f098e40af0073692dc01bb10dc1c6dc3b4
kalmai <leozverres@gmail.com>
2026-02-03 00:47:06 -0500

    add auto update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("VimEnter",{callback=function()require"lazy".update({show = false})end})
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
